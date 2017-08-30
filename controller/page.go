30456ddc7d5174571dd8b74d549691fa936f0d62
dino <crystal.dino@hotmail.com>
2017-08-30 16:00:53 +0800

    add pages

--- controller/page.go
+++ controller/page.go
@@ -16,14 +16,65 @@ func init() {
 	Server.GET("/", Home)
 	Server.GET("/login", Login)
 	Server.GET("/register", Register)
-
+	Server.GET("/news", News)
+	Server.GET("/summary", Summary)
+	Server.GET("/exchange", Exchange)
+	Server.GET("/account", Account)
+	Server.GET("/assets", Assets)
+	Server.GET("/code", Code)
 }
 
 //Home home page
 func Home(c *gin.Context) {
-	c.HTML(http.StatusOK,"home.html",gin.H{
-		"title":"crystal.dino",
+	c.HTML(http.StatusOK, "home.html", gin.H{
+		"title":  "Home",
+		"isHome": true,
 	})
+}
+
+//News news page
+func News(c *gin.Context) {
+	c.HTML(http.StatusOK, "news.html", gin.H{
+		"title":  "News",
+		"isNews": true,
+	})
+}
+
+//Summary summary page
+func Summary(c *gin.Context) {
+	c.HTML(http.StatusOK, "summary.html", gin.H{
+		"title":     "Summary",
+		"isSummary": true,
+	})
+}
+
+//Exchage exchange page
+func Exchange(c *gin.Context) {
+	c.HTML(http.StatusOK, "exchange.html", gin.H{
+		"title":      "Exchange",
+		"isExchange": true,
+	})
+}
+
+//Account account page
+func Account(c *gin.Context) {
+	c.HTML(http.StatusOK, "account.html", gin.H{
+		"title":     "Account",
+		"isAccount": true,
+	})
+}
+
+//Assets assets page
+func Assets(c *gin.Context) {
+	c.HTML(http.StatusOK, "assets.html", gin.H{
+		"title":    "Assets",
+		"isAssets": true,
+	})
+}
+
+//Code github page
+func Code(c *gin.Context) {
+	c.Redirect(http.StatusMovedPermanently, "https://github.com/CrystalDino")
 }
 
 //Login the login page
